e7ece6be658bf53a9fb9a3cb4d905a522ac66cc6
Nils Werner <nils.werner@student.kit.edu>
2021-12-21 15:44:50 +0100

    refactored td impact evaluation experiment

--- cooperative/src/bin/evaluate_td_impact.rs
+++ cooperative/src/bin/evaluate_td_impact.rs
@@ -5,63 +5,64 @@ use cooperative::dijkstra::model::PathResult;
 use cooperative::dijkstra::potentials::init_cch_potential::init_cch_potential;
 use cooperative::dijkstra::potentials::TDPotential;
 use cooperative::dijkstra::server::{CapacityServer, CapacityServerOps};
-use cooperative::experiments::queries::{generate_queries, QueryType};
 use cooperative::graph::speed_functions::bpr_speed_function;
 use cooperative::io::io_graph::load_capacity_graph;
 use cooperative::io::io_node_order::load_node_order;
-use cooperative::util::cli_args::{parse_arg_optional, parse_arg_required};
+use cooperative::io::io_queries::load_queries;
+use cooperative::util::cli_args::parse_arg_required;
 use rust_road_router::algo::TDQuery;
 use rust_road_router::datastr::graph::time_dependent::Timestamp;
-use rust_road_router::report::measure;
 use std::path::Path;
 
-/// Evaluates the impact of time-dependent queries.
+/// Evaluates the impact of time-dependence on a given set of queries.
 /// Comparison is provided by a more static setting with only 1 bucket per edge.
-/// The evaluation itself takes place on the time-dependent graph.
-/// Queries are generated randomly and either uniform, geometric or population-density-based.
+/// The evaluation itself takes place on the time-dependent graph, with several steps (100/200/300/400/600 buckets)
 ///
 /// Expected result: with a rising number of long-distance queries, the time-dependent graph
 /// should outperform the static setting (in terms of travel times).
 ///
-/// Additional parameters: <path_to_graph> <num_buckets> <num_queries> <query_mode = uniform>
+/// Additional parameters: <path_to_graph> <path_to_queries>
 fn main() -> Result<(), Box<dyn Error>> {
-    let (path, num_buckets, num_queries, query_type) = parse_args()?;
-    let graph_directory = Path::new(&path);
+    let (graph_directory, query_directory) = parse_args()?;
+    let path = Path::new(&graph_directory);
 
-    // load static graph
-    let (graph, time) = measure(|| load_capacity_graph(graph_directory, 1, bpr_speed_function).unwrap());
-    println!("Graph loaded in {} ms", time.to_std().unwrap().as_nanos() as f64 / 1_000_000.0);
+    // load queries
+    let query_path = path.join("queries").join(query_directory);
+    let queries = load_queries(&query_path)?;
+
+    let graph = load_capacity_graph(&path, 1, bpr_speed_function).unwrap();
 
     // init cch potential
-    let order = load_node_order(graph_directory)?;
+    let order = load_node_order(&path)?;
     let cch_pot_data = init_cch_potential(&graph, order);
 
-    // generate queries
-    let queries = generate_queries(&graph, query_type, num_queries);
+    let mut path_results = Vec::new();
+    let bucket_sizes = [1, 100, 200, 300, 400, 600];
 
-    // first run on time-independent graph
+    // separate treatment for first round
     let mut server = CapacityServer::new_with_potential(graph, cch_pot_data.forward_potential());
-    let paths = get_query_paths(&mut server, &queries);
+    path_results.push(get_query_paths(&mut server, &queries));
 
-    // repeat the same run with more buckets
-    let (graph, time) = measure(|| load_capacity_graph(graph_directory, num_buckets, bpr_speed_function).unwrap());
-    println!("TD-Graph loaded in {} ms", time.to_std().unwrap().as_nanos() as f64 / 1_000_000.0);
+    // determine paths for all remaining bucket settings
+    for &num_buckets in &bucket_sizes[1..] {
+        let graph = load_capacity_graph(&path, num_buckets, bpr_speed_function).unwrap();
+        server = CapacityServer::new_with_potential(graph, cch_pot_data.forward_potential());
+        path_results.push(get_query_paths(&mut server, &queries));
+    }
 
-    let mut server = CapacityServer::new_with_potential(graph, cch_pot_data.forward_potential());
-    let td_paths = get_query_paths(&mut server, &queries);
+    // evaluation based on struct with highest number of buckets
+    debug_assert_eq!(bucket_sizes.iter().max(), bucket_sizes.last());
 
-    // verify validity of results
-    assert_eq!(paths.len(), td_paths.len(), "number of valid paths should be the same!");
+    path_results.iter().enumerate().for_each(|(idx, paths)| {
+        let path_dist_sum = sum_path_lengths(paths, &server);
+        println!(
+            "Sum of path lengths for {} buckets: {} ({} valid paths)",
+            bucket_sizes[idx],
+            path_dist_sum,
+            paths.len()
+        );
+    });
 
-    let path_dist_sum = sum_path_lengths(&paths, &server);
-    let td_path_dist_sum = sum_path_lengths(&td_paths, &server);
-
-    println!("-------------------------");
-    println!("sum of distances (static): {}", path_dist_sum);
-    println!("sum of distances (dynamic): {}", td_path_dist_sum);
-    println!("-------------------------");
-
-    //TODO write csv result for further comparison
     Ok(())
 }
 
@@ -76,13 +77,11 @@ fn sum_path_lengths<Pot: TDPotential>(paths: &Vec<PathResult>, server: &Capacity
     paths.iter().map(|path| server.path_distance(path) as u128).sum()
 }
 
-fn parse_args() -> Result<(String, u32, u32, QueryType), Box<dyn Error>> {
+fn parse_args() -> Result<(String, String), Box<dyn Error>> {
     let mut args = env::args().skip(1);
 
-    let graph_directory: String = parse_arg_required(&mut args, "Graph Directory")?;
-    let num_buckets: u32 = parse_arg_required(&mut args, "number of buckets")?;
-    let num_queries: u32 = parse_arg_required(&mut args, "number of queries")?;
-    let query_mode = parse_arg_optional(&mut args, QueryType::Uniform);
+    let graph_directory = parse_arg_required(&mut args, "Graph Directory")?;
+    let query_directory = parse_arg_required(&mut args, "Query Directory")?;
 
-    Ok((graph_directory, num_buckets, num_queries, query_mode))
+    Ok((graph_directory, query_directory))
 }
